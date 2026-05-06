2269fe59dc926adfc58be21d89ae782e3bd4576a
Dominik Steenken <dost@de.ibm.com>
2026-05-06 10:56:28 +0200

    float to string patches

--- stdlib/public/core/FloatingPointToString.swift
+++ stdlib/public/core/FloatingPointToString.swift
@@ -1694,7 +1694,31 @@ fileprivate func _finishFormatting(
 }
 
 // Table with ASCII strings for all 2-digit decimal numbers.
-// Stored as little-endian UInt16s for efficiency
+// Stored as UInt16s for efficiency
+#if _endian(big)
+fileprivate let asciiDigitTable: _InlineArray<100, UInt16> = [
+  0x3030, 0x3031, 0x3032, 0x3033, 0x3034,
+  0x3035, 0x3036, 0x3037, 0x3038, 0x3039,
+  0x3130, 0x3131, 0x3132, 0x3133, 0x3134,
+  0x3135, 0x3136, 0x3137, 0x3138, 0x3139,
+  0x3230, 0x3231, 0x3232, 0x3233, 0x3234,
+  0x3235, 0x3236, 0x3237, 0x3238, 0x3239,
+  0x3330, 0x3331, 0x3332, 0x3333, 0x3334,
+  0x3335, 0x3336, 0x3337, 0x3338, 0x3339,
+  0x3430, 0x3431, 0x3432, 0x3433, 0x3434,
+  0x3435, 0x3436, 0x3437, 0x3438, 0x3439,
+  0x3530, 0x3531, 0x3532, 0x3533, 0x3534,
+  0x3535, 0x3536, 0x3537, 0x3538, 0x3539,
+  0x3630, 0x3631, 0x3632, 0x3633, 0x3634,
+  0x3635, 0x3636, 0x3637, 0x3638, 0x3639,
+  0x3730, 0x3731, 0x3732, 0x3733, 0x3734,
+  0x3735, 0x3736, 0x3737, 0x3738, 0x3739,
+  0x3830, 0x3831, 0x3832, 0x3833, 0x3834,
+  0x3835, 0x3836, 0x3837, 0x3838, 0x3839,
+  0x3930, 0x3931, 0x3932, 0x3933, 0x3934,
+  0x3935, 0x3936, 0x3937, 0x3938, 0x3939
+]
+#else
 fileprivate let asciiDigitTable: _InlineArray<100, UInt16> = [
   0x3030, 0x3130, 0x3230, 0x3330, 0x3430,
   0x3530, 0x3630, 0x3730, 0x3830, 0x3930,
@@ -1717,21 +1741,29 @@ fileprivate let asciiDigitTable: _InlineArray<100, UInt16> = [
   0x3039, 0x3139, 0x3239, 0x3339, 0x3439,
   0x3539, 0x3639, 0x3739, 0x3839, 0x3939
 ]
+#endif
 
-// The constants below assume we're on a little-endian processor
 fileprivate func _infinity(
   buffer: inout MutableRawSpan,
   sign: FloatingPointSign
 ) -> Range<Int> {
   if sign == .minus {
+    var data: UInt32 = 0x666e692d // "-inf"
+    #if _endian(big)
+    data = data.byteSwapped
+    #endif
     buffer.storeBytes(
-      of: 0x666e692d, // "-inf"
+      of: data, // "-inf"
       toByteOffset: 0,
       as: UInt32.self)
     return 0..<4
   } else {
+    var data: UInt32 = 0x00666e69 // "inf\0"
+    #if _endian(big)
+    data = data.byteSwapped
+    #endif
     buffer.storeBytes(
-      of: 0x00666e69, // "inf\0"
+      of: data, // "inf\0"
       toByteOffset: 0,
       as: UInt32.self)
     return 0..<3
@@ -1743,14 +1775,22 @@ fileprivate func _zero(
   sign: FloatingPointSign
 ) -> Range<Int> {
   if sign == .minus {
+    var data: UInt32 = 0x302e302d // "-0.0"
+    #if _endian(big)
+    data = data.byteSwapped
+    #endif
     buffer.storeBytes(
-      of: 0x302e302d, // "-0.0"
+      of: data, // "-0.0"
       toByteOffset: 0,
       as: UInt32.self)
     return 0..<4
   } else {
+    var data: UInt32 = 0x00302e30 // "0.0\0"
+    #if _endian(big)
+    data = data.byteSwapped
+    #endif
     buffer.storeBytes(
-      of: 0x00302e30, // "0.0\0"
+      of: data, // "0.0\0"
       toByteOffset: 0,
       as: UInt32.self)
     return 0..<3
@@ -1871,7 +1911,11 @@ fileprivate func _intToEightDigits(_ n: UInt32) -> UInt64 {
   let singles = div2 | (mod2 &<< 8)
 
   // Convert 8 digits to ASCII characters
+#if _endian(big)
+  return (singles &+ 0x3030303030303030).byteSwapped
+#else
   return singles &+ 0x3030303030303030
+#endif
 }
 
 // ================================================================
